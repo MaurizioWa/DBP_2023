9c3347dde43895bf06ac9ad12dea893b1c6b4824
Maurizio Daniel Luque Soto <132947833+MaurizioWa@users.noreply.github.com>
2023-11-22 18:05:14 -0500

    Add files via upload

---
+++ FormCV/CV.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormCV
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CV : ContentPage
+    {
+        public CV(string nombres, string apellidos, string correo, string ocupacion, string telefono)
+        {
+            InitializeComponent();
+
+            nombresLabel.Text = nombres;
+            apellidosLabel.Text = apellidos;
+            correoLabel.Text = correo;
+            ocupacionLabel.Text = ocupacion;
+            telefonoLabel.Text = telefono;
+        }
+    }
+}
---
+++ FormCV/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormCV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSubmitClicked(object sender, EventArgs e)
+        {
+            var nombres = nombresEntry.Text;
+            var apellidos = apellidosEntry.Text;
+            var correo = correoEntry.Text;
+            var ocupacion = ocupacionEntry.Text;
+            var telefono = telefonoEntry.Text;
+
+            var cvPage = new CV(nombres, apellidos, correo, ocupacion, telefono);
+            await Navigation.PushAsync(cvPage);
+        }
+
+    }
+}
